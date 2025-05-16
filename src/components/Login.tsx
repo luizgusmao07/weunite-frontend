d05774c8e7865f880cf686a5c29ff7010d94fed0
Matheus Oliveira <matheusoliveirale2007@gmail.com>
2025-05-16 12:19:17 -0300

    fix: removing CardComponent from Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
