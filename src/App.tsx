d93163997e42b697a0c28a939c95da8922d817ab
Matheus Oliveira <matheusoliveirale2007@gmail.com>
2025-05-16 12:17:48 -0300

    issue #004 feat: adding forms login

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import './App.css'
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import AuthPage from './pages/AuthPage';
+import AuthPage from "./pages/AuthPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-
   return (
     <>
       <Routes>
         <Route path={"/auth"} element={<AuthPage />} />
+        <Route path={"/login"} element={<LoginPage />} /> 
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
