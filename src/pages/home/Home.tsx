5096cdf0cbcb385599d2c1bcb4ca91073e67ec12
Marcus Roberto Santos Alves <marcusroberto900@gmail.com>
2025-06-28 18:08:27 -0300

    feat: adicionar barra lateral de oportunidades e atualizar página Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,17 @@
 
+import { OpportunitiesSidebar } from '../../components/home/OpportunitiesSidebar';
+
 export function Home() {
   return (
-    <p>Home</p>
+    <div className="relative min-h-screen">
+      {/* Main content */}
+      <div className="pr-80"> {/* Add padding-right to make space for the sidebar */}
+        <p>Home</p>
+        {/* Add your main content here */}
+      </div>
+      
+      {/* Fixed sidebar on the right */}
+      <OpportunitiesSidebar />
+    </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
         target: "http://localhost:8080",
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => path,
       },
     },
   },
