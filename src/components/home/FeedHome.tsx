90e8323dda07a3e6826f887799a422613c3436fa
mathessoliv <matheusoliveirale2007@gmail.com>
2025-08-09 13:16:30 -0300

    feat: adicionando skeleton enquanto o post carrega

--- src/components/home/FeedHome.tsx
+++ src/components/home/FeedHome.tsx
@@ -1,23 +1,36 @@
 import type { Post as PostType } from "@/@types/post.types";
 import Post from "@/components/post/Post";
+import PostSkeleton from "@/components/post/PostSkeleton";
 import { useGetPosts } from "@/state/usePosts";
 
 export function FeedHome() {
-  const { data } = useGetPosts();
+  const { data, isLoading } = useGetPosts();
   const posts = data?.data;
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center w-full">
+        <div className="max-w-[700px] w-full flex flex-col items-center">
+          {Array.from({ length: 3 }).map((_, index) => (
+            <PostSkeleton key={index} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 
   if (!posts || posts.length === 0) {
     return (
-      <div className="flex justify-center items-center min-h-[80vh]r">
+      <div className="flex justify-center items-center min-h-[80vh]">
         <p className="text-muted-foreground">Nenhum post encontrado</p>
       </div>
     );
   }
 
   return (
-    <div className="flex justify-center w-full ">
-      <div className="max-w-[700px] w-full  flex flex-col items-center">
-        {posts.map((post: PostType,) => (
+    <div className="flex justify-center w-full">
+      <div className="max-w-[700px] w-full flex flex-col items-center">
+        {posts.map((post: PostType) => (
           <Post key={post.id} post={post} />
         ))}
       </div>
---
+++ src/components/post/PostSkeleton.tsx
@@ -0,0 +1,52 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function PostSkeleton() {
+  return (
+    <Card className="w-full max-w-[45em] shadow-none border-0 border-b rounded-none border-foreground/50">
+      <CardHeader className="flex flex-row items-center gap-4">
+
+        <Skeleton className="h-10 w-10 rounded-full" />
+        
+        <div className="flex flex-col space-y-2 flex-1">
+          <Skeleton className="h-4 w-24" />
+          <Skeleton className="h-3 w-16" />
+        </div>
+
+        <Skeleton className="h-5 w-5 rounded-full ml-auto" />
+      </CardHeader>
+
+      <CardContent className="space-y-4 mt-[-18px]">
+        {Math.random() > 0.5 && (
+          <Skeleton className="w-full h-[200px] rounded-md" />
+        )}
+        
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-3/4" />
+        </div>
+      </CardContent>
+
+      <CardFooter className="flex flex-col mt-[-20px]">
+        <div className="flex justify-between w-full mb-3">
+          <Skeleton className="h-4 w-32" />
+        </div>
+
+        <div className="flex w-full justify-between">
+          <div className="flex items-center gap-3">
+            <Skeleton className="h-5 w-5 rounded-full" />
+            <Skeleton className="h-5 w-5 rounded-full" />
+            <Skeleton className="h-5 w-5 rounded-full" />
+          </div>
+          <Skeleton className="h-5 w-5 rounded-full" />
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
